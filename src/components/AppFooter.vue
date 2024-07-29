1d87a13065b8f558ccfd82fb5a11b1f95a557608
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-30 00:44:44 +0200

    [UPD] created layout to AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,30 @@
-<template lang="">
+<template>
     <div>
-        
+        <!-- top footer -->
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="row">
+                        <div class="col-4"></div>
+                        <div class="col-4"></div>
+                        <div class="col-4"></div>
+                        <div class="col-4"></div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- bottom footer -->
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
 <script>
 export default {
     
