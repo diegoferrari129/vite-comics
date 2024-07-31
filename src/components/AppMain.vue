5592a5ed35dadbed07e840d57ecf86e4c9e7e64d
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-31 04:01:35 +0200

    [UPD] added jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,21 @@
 <script>
+
 export default {
     
 }
 </script>
 
 <template>
+    <div class="jumbotron">
+        <div class="row"></div>
+    </div>
     <main class="main">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <div class="main__content text-white">
-                        content
+                <div class="col-2">
+                    <div class="main__content">
+                        <img src="" alt="">
+                        <h6>content</h6>
                     </div>
                 </div>
             </div>
@@ -19,7 +24,18 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+    .jumbotron {
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
+        min-height: 400px;
+    }
+
     .main {
         background-color: black;
+
+        .main__content {
+            color: white;
+        }
     }
+
 </style>
